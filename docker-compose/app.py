6a4b9b61a5174b6b572ab5c7eb783314b813aa75
QuatZo <48104274+QuatZo@users.noreply.github.com>
2020-12-06 01:32:58 +0100

    Refactoring + Bug Fixes + Clip 0029

--- docker-compose/app.py
+++ docker-compose/app.py
@@ -12,6 +12,7 @@ import statistics
 import os
 import json
 import pandas as pd
+import plotly.io as pltio
 import plotly.express as px
 import statsmodels
 
@@ -22,20 +23,22 @@ from helpers import Helpers
 
 app = Flask(__name__)
 celery = Celery(broker='redis://redis:6379/0', backend='redis://redis:6379/0')
-celery.conf.broker_transport_options = {"visibility_timeout": 3600 * 24 * 360} # 1h * 24 * 360 = 360d
+celery.conf.broker_transport_options = {
+    "visibility_timeout": 3600 * 24 * 360}  # 1h * 24 * 360 = 360d
 
 
-def read_json(path_to_json = 'data/input/'):
+def read_json(path_to_json='data/input/'):
     """
-       Reads jsons in the specified dir
-        Parameters
-        ----------
-            path_to_json : string
+    Reads jsons in the specified dir
+
+    Parameters
+    ----------
+        path_to_json : string
 
 
-        Returns
-        -------
-            List of jsons.
+    Returns
+    -------
+        List of jsons.
     """
     data = []
     for file_name in [file for file in os.listdir(path_to_json) if file.endswith('openaigym.episode_batch.stats.json')]:
@@ -43,26 +46,28 @@ def read_json(path_to_json = 'data/input/'):
             data.append(json.load(json_file))
             return data
 
-def parse_json_2_dataframe(jsons,unwanted_col = 'unwanted_col'):
+
+def parse_json_2_dataframe(jsons, unwanted_col='unwanted_col'):
     """
-       Reads jsons in the specified dir
-        Parameters
-        ----------
-            jsons : List of jsons.
+    Reads jsons in the specified dir
 
-            list_of_cols : list of columns from which DataFrame is made.
+    Parameters
+    ----------
+        jsons : List of jsons.
 
-            unwanted_col : OPTIONAL, str.
+        list_of_cols : list of columns from which DataFrame is made.
 
-            dataframe_aborted_cols : OPTIONAL, list of aborted columns
+        unwanted_col : OPTIONAL, str.
+
+        dataframe_aborted_cols : OPTIONAL, list of aborted columns
 
 
 
-        Returns
-        -------
-            cleaned_dataframe : pd.DataFrame    N long table, N is number of episodes in the data.
+    Returns
+    -------
+        cleaned_dataframe : pd.DataFrame    N long table, N is number of episodes in the data.
 
-            param_config_df : pd.DataFrame      17x1 table with parameter values
+        param_config_df : pd.DataFrame      17x1 table with parameter values
     """
     for json in jsons:
         list_of_cols = []
@@ -70,48 +75,53 @@ def parse_json_2_dataframe(jsons,unwanted_col = 'unwanted_col'):
         for key in json:
             if not isinstance(json[key], float):
                 if key != unwanted_col:
-                    col = pd.Series(json[key],name=key)
+                    col = pd.Series(json[key], name=key)
                     list_of_cols.append(col)
                 else:
-                    dataframe_aborted_cols.append(pd.Series(json[key],name=key))
+                    dataframe_aborted_cols.append(
+                        pd.Series(json[key], name=key))
 
         dataframe = pd.DataFrame(list_of_cols).T
         cleaned_dataframe = dataframe[['timestamps', 'episode_lengths', 'episode_rewards',
-           'episode_winners', 'episode_types']].dropna(axis=0)
-        config_and_best = dataframe[['episode_best','config']]
+                                       'episode_winners', 'episode_types']].dropna(axis=0)
+        config_and_best = dataframe[['episode_best', 'config']]
 
-        config = config_and_best.transpose()[['game', 'hiding', 'seeker', 'video']].drop('episode_best', axis=0)
-
+        config = config_and_best.transpose(
+        )[['game', 'hiding', 'seeker', 'video']].drop('episode_best', axis=0)
 
         for params in [config[col] for col in config.columns]:
-                for val in params.values:
-                    df = pd.DataFrame(list(val.items())).set_index(0).transpose()
-                    if params.name == 'hiding':
-                        hiding_df = df
-                    elif params.name == 'seeker':
-                        seeker_df = df
-                        seeker_df = seeker_df.join(hiding_df,rsuffix='_hiding',lsuffix='_seeker')
-                    elif params.name == 'video':
-                        video_df = df
-                    elif params.name == 'game':
-                        game_df = df
-        seeker_df = seeker_df.merge(game_df,left_index=True, right_index=True)
-        param_config_df = seeker_df.merge(video_df,left_index=True, right_index=True)
+            for val in params.values:
+                df = pd.DataFrame(list(val.items())).set_index(0).transpose()
+                if params.name == 'hiding':
+                    hiding_df = df
+                elif params.name == 'seeker':
+                    seeker_df = df
+                    seeker_df = seeker_df.join(
+                        hiding_df, rsuffix='_hiding', lsuffix='_seeker')
+                elif params.name == 'video':
+                    video_df = df
+                elif params.name == 'game':
+                    game_df = df
+        seeker_df = seeker_df.merge(game_df, left_index=True, right_index=True)
+        param_config_df = seeker_df.merge(
+            video_df, left_index=True, right_index=True)
         return cleaned_dataframe, param_config_df
+
 
 def parse_episode_rewards(episode_reward):
     """
-           Reads jsons in the specified dir
-            Parameters
-            ----------
-                episode_reward : pd.Series  containing list of lists
+    Reads jsons in the specified dir
 
-            Returns
-            -------
-                sum(seeker_sum) : int   sum of seeker rewards
+    Parameters
+    ----------
+        episode_reward : pd.Series  containing list of lists
 
-                sum(hiding_sum) : int      sum of hiding rewards
-        """
+    Returns
+    -------
+        sum(seeker_sum) : int   sum of seeker rewards
+
+        sum(hiding_sum) : int      sum of hiding rewards
+    """
     seeker_sum, hiding_sum = [], []
     for frame in episode_reward:
         for seeker_reward, hiding_reward in frame:
@@ -119,14 +129,15 @@ def parse_episode_rewards(episode_reward):
             hiding_sum.append(hiding_reward)
     return sum(seeker_sum), sum(hiding_sum)
 
-def plot_rewards(data,agent,path_to_save):
 
+def plot_rewards(data, agent, path_to_save):
     df = data.copy()
-    df.episode_rewards = [parse_episode_rewards(episode_reward) for episode_reward in zip(df.episode_rewards)]
+    df.episode_rewards = [parse_episode_rewards(
+        episode_reward) for episode_reward in zip(df.episode_rewards)]
     agents_rewards = df.episode_rewards.apply(pd.Series)
     df['seeker_rewards'] = agents_rewards[0]
     df['hiding_rewards'] = agents_rewards[1]
-    df.drop('episode_rewards',axis=1,inplace=True)
+    df.drop('episode_rewards', axis=1, inplace=True)
     df["episode_winners"] = df["episode_winners"].astype('category')
     df["episode_winners_cat"] = df["episode_winners"].cat.codes
 
@@ -136,11 +147,12 @@ def plot_rewards(data,agent,path_to_save):
         m.groupby([m, (~m).cumsum().where(m)]).cumcount().add(1).mul(m))
     df['hiding_win'] = (
         m.groupby([n, (~n).cumsum().where(n)]).cumcount().add(1).mul(n))
-    df['consecutive_result'] = df[['hiding_win','seeker_win']].max(axis=1)
-    df.drop('timestamps',inplace=True,axis=1)
+    df['consecutive_result'] = df[['hiding_win', 'seeker_win']].max(axis=1)
+    df.drop('timestamps', inplace=True, axis=1)
 
     agent_data = df[df['episode_winners'] == agent].copy()
-    agent_data = agent_data[agent_data['consecutive_result'] >= agent_data['consecutive_result'].mean()]
+    agent_data = agent_data[agent_data['consecutive_result']
+                            >= agent_data['consecutive_result'].mean()]
     agent_data['std_hiding_rewards'] = agent_data['hiding_rewards'].std()
     agent_data['std_seeker_rewards'] = agent_data['seeker_rewards'].std()
 
@@ -148,16 +160,27 @@ def plot_rewards(data,agent,path_to_save):
         fig = px.scatter(agent_data, x=agent_data.index, y=['seeker_rewards', 'hiding_rewards'], width=600, height=400,
                          size='consecutive_result',
                          labels=dict(index="Episode", value="Amount of Points", variable='Colors',
-                                     consecutive_result='Consecutive_result', episode_winners='Episode_winners'),title=str("winner: " + agent),
-                         hover_data=['episode_winners','std_seeker_rewards','std_hiding_rewards'],trendline="lowess")
-        with open(os.path.join(path_to_save,agent+'_wins_plot.html'),'w+') as f:
-            fig.write_html(fig,f,auto_open=False)
+                                     consecutive_result='Consecutive_result', episode_winners='Episode_winners'), title=str("winner: " + agent),
+                         hover_data=['episode_winners', 'std_seeker_rewards', 'std_hiding_rewards'], trendline="lowess")
+        with open(os.path.join(path_to_save, agent+'_wins_plot.html'), 'w+') as f:
+            pltio.write_html(fig, f, auto_open=False)
 
-        return fig.to_html(include_plotlyjs=False,full_html=False)
+        fig.update_layout(
+            legend=dict(
+                orientation="h",
+                yanchor="bottom",
+                y=1.02,
+                xanchor="right",
+                x=1
+            ),
+            width=500,
+        )
+
+        return fig.to_html(include_plotlyjs=False, full_html=False)
 
     else:
         text = 'No winnings for ' + agent.lower() + ' agent'
-        return "<div class='display-4'> " + text + " </div>"
+        return "<div class='display-2'> " + text + " </div>"
 
 
 @celery.task(name='train.core', bind=True)
@@ -225,7 +248,7 @@ def train(self, core_id, config_data, start_date):
             algorithm.before_action(obs_n=obs_n)
 
             action_n = algorithm.take_action(obs_n=obs_n)
-            obs_old_n=copy.deepcopy(obs_n)
+            obs_old_n = copy.deepcopy(obs_n)
 
             algorithm.before_step(action_n=action_n)
             obs_n, reward_n, done, _ = env.step(action_n)
@@ -235,7 +258,6 @@ def train(self, core_id, config_data, start_date):
                 obs_n=obs_n,
                 done=done
             )
-
 
             Helpers.update_img_status(
                 env, cfg['video']['monitoring'], step_img_path, render_mode)
@@ -258,10 +280,11 @@ def train(self, core_id, config_data, start_date):
     Helpers.cleanup(env, core_id)
 
     metadata = {
-        'step' : 'read meta file'
+        'step': 'read meta file'
     }
     self.update_state(state='PLOTTING', meta=metadata)
-    path_to_file = '/opt/app/monitor/' + start_date + "/core-" + str(core_id) + '/'
+    path_to_file = '/opt/app/monitor/' + \
+        start_date + "/core-" + str(core_id) + '/'
     raw_stats = read_json(path_to_file)
 
     metadata = {
